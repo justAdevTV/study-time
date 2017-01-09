b0153f40eb86f13a65abb0f6b1d707cd5d6c3935
Justin <justpunz@live.com>
2017-01-09 11:09:15 -0800

    Fixed Toggle

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+	
+	chrome.storage.sync.get('isStudying', function(res){
+		var isStudying = res.isStudying;
+
+		if (isStudying) {
+			document.getElementById('study_button').innerHTML = "Stop";
+		} else {
+			document.getElementById('study_button').innerHTML = "Start";
+		}
+
+	});	    
+
     var link = document.getElementById('study_button');
     // onClick's logic below:
     link.addEventListener('click', function() { 
@@ -14,10 +26,10 @@ function toggleStudyMode() {
 
 		if (isStudying) {
 			isStudying = false;
-			document.getElementById('study_button').innerHTML = "Stop Studying";
+			document.getElementById('study_button').innerHTML = "Stop";
 		} else {
 			isStudying = true;
-			document.getElementById('study_button').innerHTML = "Start Studying";
+			document.getElementById('study_button').innerHTML = "Start";
 		}
 
 	    chrome.storage.sync.set({'isStudying': isStudying});
